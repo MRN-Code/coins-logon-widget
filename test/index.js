367801c50ffc88706d91d1dc08a86b62b4f54af6
Cory Reed <creed@mrn.org>
2015-12-21 13:41:35 -0800

    Refactor test code.
    This adds widget setup and teardown methods to a test utility file and
    moves the widget field setting method there, too. It also removes unneeded
    markup from the test HTML.

--- test/index.js
+++ test/index.js
@@ -3,42 +3,39 @@ var CoinsLogonWidget = require('../scripts/coins-logon-widget.js');
 var html = require('html!./index.html');
 var jQuery = require('jquery');
 var Promise = this.Promise = require('bluebird'); // phantomJS polyfill. seriously. :-|
+var testUtils = require('./test-utils');
+
 jQuery(window.document.body).append(html);
-
-var server;
-
-var setWidgetFields = function(opts) {
-    var widget = opts.widget;
-    widget.form.formGroups[0].element.children[1].value = opts.username;
-    widget.form.formGroups[1].element.children[1].value = opts.password;
-};
 
 test('constructor, basic', function(t) {
     var el = document.getElementById('regular_form');
-    var myWidget = new CoinsLogonWidget({
+    var myWidget = testUtils.widgetFactory({
         el: el,
-        baseUrl: 'coins-api-root',
-        authCookieName: 'coins-cookie-nom-noms',
     });
+
     t.ok(myWidget, 'constructor returns a widget');
     t.equal(myWidget.element, el, 'constructor sets el');
     t.ok(jQuery('#regular_form.coins-logon-widget:visible').length, 'form visible on DOM');
+
+    testUtils.teardownWidget(myWidget);
+
     t.end();
 });
 
 test('constructor, hiddenLabels', function(t) {
     var el = document.getElementById('hidden_labels_form');
-    var myWidget = new CoinsLogonWidget({
+    var myWidget = testUtils.widgetFactory({
         el: el,
-        baseUrl: 'coins-api-root',
-        authCookieName: 'coins-cookie-nom-noms',
-        hiddenLabels: true
+        hiddenLabels: true,
     });
+
     t.equal(
         2,
         jQuery('#hidden_labels_form .coins-logon-widget-label').length,
         'hidden label DOMs nodes present for both un and pw inputs'
     );
+
+    testUtils.teardownWidget(myWidget);
 
     // visuallyhidden class doesn't actually hide elements, so omit doing
     // vis test.  x-browser :visible JQ query selector seems to have varying
@@ -55,10 +52,8 @@ test('field order', function(t) {
     var myModule = {
         showWidget: function() {
             $target.append('<div id="' + tempId + '"></div>');
-            myModule.logonWidget = new CoinsLogonWidget({
+            myModule.logonWidget = testUtils.widgetFactory({
                 el: document.getElementById(tempId),
-                baseUrl: 'coins-api-root',
-                authCookieName: 'delicious-cookies',
             });
             myModule.logonWidget.removeAllListeners();
             $target.fadeIn();
@@ -86,13 +81,15 @@ test('field order', function(t) {
     myModule.showWidget();
     t.equal(getFirstInputName(), 'username', 'User input is still first');
 
+    testUtils.teardownWidget(myModule.logonWidget);
+
     t.end();
 });
 
 test('api activity', {timeout: 2000}, function(t) {
     t.plan(1);
     var el = document.getElementById('api_call_form');
-    var myWidget = new CoinsLogonWidget({
+    var myWidget = testUtils.widgetFactory({
         el: el,
         baseUrl: 'http://localhost:12354',
         authCookieName: 'CAS_Auth_User', // @TODO, test when i figure out how to set x-domain cookies
@@ -102,11 +99,14 @@ test('api activity', {timeout: 2000}, function(t) {
         username: 'someun',
         password: 'somepassword',
     };
-    setWidgetFields(wigetData);
+    testUtils.setWidgetFields(wigetData);
     Promise.resolve(myWidget.login(wigetData))
     .then(function() {
         t.equal(jQuery('#api_call_form button').text(), 'Log Out', 'login cycle successful');
     })
     .catch(t.fail)
-    .then(t.end);
+    .then(function() {
+        testUtils.teardownWidget(myWidget);
+        t.end();
+    });
 });
